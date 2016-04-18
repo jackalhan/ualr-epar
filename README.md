30f3c7f5198e7949baf5184312bc4ebcf87d8244
jackalhan <jackalhan@gmail.com>
2016-04-18 11:44:11 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# e-par
+Electronic PAR
---
+++ src/test/java/com/jackalhan/UalrEparApplicationTests.java
@@ -0,0 +1,18 @@
+package com.jackalhan;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = UalrEparApplication.class)
+@WebAppConfiguration
+public class UalrEparApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
